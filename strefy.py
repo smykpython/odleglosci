014ec8cd63ad78e1fa8be650db2cac5837323137
Smyk Rafał <Smyk.Rafal@zabka.pl>
2021-10-25 16:34:57 +0200

    zmiana

--- strefy.py
+++ strefy.py
@@ -131,7 +131,7 @@ if lons >14 and lats>46 and range1 > 100:
         st.write(locit_geo.head(4))
 
         #locit_3city = gpd.sjoin(locit_geo, poli_isochron[['index','geometry']], how='inner', predicate='intersects')
-        locit_3city = gpd.sjoin(locit_geo, poli_isochron, how='inner')#, predicate='intersects')
+        locit_3city = gpd.sjoin(locit_geo, poli_isochron, how="inner", predicate='intersects')#, predicate='intersects')
         
         st.write(locit_3city_1000.head(4))        
 
