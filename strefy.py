6f6b9d493e71629cbabc60ecd14a5b1b6a99c049
Smyk Rafał <Smyk.Rafal@zabka.pl>
2021-10-25 22:59:53 +0200

    sjoin

--- strefy.py
+++ strefy.py
@@ -136,9 +136,9 @@ if lons >14 and lats>46 and range1 > 100:
         st.text(locit_geo.head(2))
 
         #locit_3city = gpd.sjoin(locit_geo, poli_isochron[['index','geometry']], how='inner', predicate='intersects')
-        #locit_3city = gpd.sjoin(locit_geo, poli_isochron, how='inner', predicate='intersects')#, predicate='intersects')
-        locit_3city = gpd.sjoin(locit_geo, poli_isochron)
-        locit_3city.rename(columns={'Unnamed: 0':'index'}, inplace=True)
+        locit_3city = gpd.sjoin(locit_geo, poli_isochron, how='inner',  lsuffix='', rsuffix='right')#, predicate='intersects')
+        #locit_3city = gpd.sjoin(locit_geo, poli_isochron)
+        #locit_3city.rename(columns={'Unnamed: 0':'index'}, inplace=True)
         st.text(locit_3city.head(4))
         locit_3city_1000 = locit_3city.dissolve(by='index', aggfunc={'POPULACJA': 'sum','POPULACJA_20_44':'sum','LICZBA_GOSPODARSTW': 'sum', 'LICZB_LOKALI_MIESZKALNYCH':'sum'})
         st.text(locit_3city_1000.head(4))
